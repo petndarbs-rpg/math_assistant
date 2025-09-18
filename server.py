e558db59eff41551471db94fe1ebc71352b5cfce
petndarbs-rpg <petndarbs@gmail.com>
2025-09-18 12:55:51 +0300

    Update server.py

--- server.py
+++ server.py
@@ -163,4 +163,4 @@ def sinteze():
         return jsonify({"error": f"Kļūda: {str(e)}"}),500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
